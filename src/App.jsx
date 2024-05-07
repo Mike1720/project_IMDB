d3ec2de08c03e72efc48c7a0cd968aedae551ba9
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2024-05-07 17:03:43 -0600

    feat: Route modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// Styles
 import './App.css'
+// COMPONENTS
+import { Header } from './layouts/Header/Header'
+import { Footer } from "./layouts/Footer/Footer";
+// REACT
+import { RoutesIndex } from './routes/RoutesIndex'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <RoutesIndex />
+            <Footer/>
+        </>
+    )
 }
 
 export default App
--- src/routes/RoutesIndex.jsx
+++ src/routes/RoutesIndex.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home } from "../pages/Home/Home.jsx";
+import { Home } from '../pages/Home/Home.jsx';
 import { NotFound } from "../pages/NotFound/NotFound.jsx";
-import { DetailSong } from '../pages/DetailSong/DetailSong.jsx';
+import { Search } from "../pages/Search/Search.jsx";
 
 export const RoutesIndex = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/detail-song' element={<DetailSong />} />
+      <Route path='/search' element={< Search />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
